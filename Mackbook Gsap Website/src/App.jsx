fd87236f75256de4caa512e0a5e8b20af2675c06
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-01-07 15:08:13 +0530

    feat: add ProductViewer component to App layout

--- Mackbook Gsap Website/src/App.jsx
+++ Mackbook Gsap Website/src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import ProductViewer from "./components/ProductViewer"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
